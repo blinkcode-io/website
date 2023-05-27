2d3d20ef6f3623979b2acf6e78ddeb22158cf1ec
Thuhaa <josephthuha@gmail.com>
2023-05-27 21:10:32 +0300

    feat: add basic layout

--- src/components/Banner.svelte
+++ src/components/Banner.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
-
 </script>
+
 <section class="hero is-primary is-halfheight mt-0">
-    <div class="hero-body">
-        <div class="">
-            <p class="title">
-                Half height hero
-            </p>
-            <p class="subtitle">
-                Half height subtitle
-            </p>
-        </div>
-    </div>
+	<div class="hero-body">
+		<div class="">
+			<p class="title">Half height hero</p>
+			<p class="subtitle">Half height subtitle</p>
+		</div>
+	</div>
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import Header from '../components/Header.svelte';
-    import Banner from "../components/Banner.svelte";
-    import InfoSection from "../components/InfoSection.svelte";
-    import Footer from "../components/Footer.svelte";
+	import Banner from '../components/Banner.svelte';
+	import InfoSection from '../components/InfoSection.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <Header />.
 <Banner />
 <InfoSection />
 <Footer />
+
 <style>
 </style>
