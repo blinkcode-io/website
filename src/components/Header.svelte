01ba95d35514972fdf8fbd30b9bbd7c3b517ae14
Joseph Kariuki <joehene@gmail.com>
2023-05-28 12:30:16 +0300

    feat:add landing page placeholders

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -24,17 +24,19 @@
 		<div class="navbar-start">
 			<a class="navbar-item"> Home </a>
 
-			<a class="navbar-item"> Documentation </a>
+			<a class="navbar-item"> About </a>
+			<a class="navbar-item"> Services </a>
+			<a class="navbar-item"> Contact </a>
 
 			<div class="navbar-item has-dropdown is-hoverable">
-				<a class="navbar-link"> More </a>
+				<a class="navbar-link"> Featured </a>
 
 				<div class="navbar-dropdown">
-					<a class="navbar-item"> About </a>
-					<a class="navbar-item"> Jobs </a>
-					<a class="navbar-item"> Contact </a>
+					<a class="navbar-item"> Support and Consulting </a>
+					<a class="navbar-item"> Tips and Tricks </a>
+					<a class="navbar-item"> Meet the Team</a>
 					<hr class="navbar-divider" />
-					<a class="navbar-item"> Report an issue </a>
+					<a class="navbar-item"> Products </a>
 				</div>
 			</div>
 		</div>
--- src/components/InfoSection.svelte
+++ src/components/InfoSection.svelte
@@ -1,24 +1,187 @@
 <script lang="ts">
+	// import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js'
+	// import 'bulma-carousel/dist/css/bulma-carousel.min.css'
+	//
+	// const carousel = bulmaCarousel.attach(
+	// 		'.carousel',
+	// )
+	// // To access to bulmaCarousel instance of an element
+	// const element = document.querySelector('#slider2');
+	// if (element && element.bulmaCarousel) {
+	// 	// bulmaCarousel instance is available as element.bulmaCarousel
+	// }
 </script>
 
 <section class="section is-medium">
-	<h1 class="title">Medium section</h1>
-	<h2 class="subtitle">
-		A simple container to divide your page into <strong>sections</strong>, like the one you're
-		currently reading.
-	</h2>
+	<h1 class="title has-text-centered m-auto=">ABOUT</h1>
+	<div class="columns mt-2">
+		<div class="column">
+			<h1 class="title">Who Are We?</h1>
+			<h2 class="subtitle">
+				We are experts at creating tailored solutions that boost productivity and business expansion. To provide our
+				customers with top-notch goods and services, our team of skilled engineers and analysts employs creative
+				problem-solving strategies.
+			</h2>
+		</div>
+		<div class="column">
+			<figure class="image is-16by9">
+				<iframe class="has-ratio" width="640" height="360" src="https://www.youtube.com/embed/YE7VzlLtp-4" frameborder="0" allowfullscreen></iframe>
+			</figure>
+		</div>
+	</div>
 </section>
 <section class="section is-medium">
-	<h1 class="title">Medium section</h1>
-	<h2 class="subtitle">
-		A simple container to divide your page into <strong>sections</strong>, like the one you're
-		currently reading.
-	</h2>
+	<h1 class="title has-text-centered m-auto=">SERVICES</h1>
+	<div class="columns is-centered">
+		<div class="column card is-two-sixths m-2">
+			<h4 class="title is-size-5 has-text-centered">
+				WEB APPS
+			</h4>
+			<div class="card-image">
+				<figure class="image is-4by3">
+					<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+				<div class="card-content">
+					We offer web application development services, and because we have exceptional domain experience and stay
+					current with industry trends, we can build reliable web apps that bring value to our clients' daily
+					operations.
+				</div>
+		</div>
+		<div class="column card is-two-sixths m-2">
+				<h4 class="title is-size-5 has-text-centered">
+					GIS
+				</h4>
+			<div class="card-image">
+				<figure class="image is-4by3">
+					<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+			<div class="card-content">
+				We offer geospatial solutions that range from corporate-wide, spatially enabled business systems to
+				custom desktop programs made for each user. Depending on the requirements of the business model, internal
+				networks, intranets, and Internet thin client protocols are effectively exploited.
+			</div>
+		</div>
+		<div class="column card is-two-sixths m-2">
+				<h4 class="title is-size-5 has-text-centered">
+					CMS
+				</h4>
+			<div class="card-image">
+				<figure class="image is-4by3">
+					<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+				</figure>
+			</div>
+			<div class="card-content">
+				We offer web application development services, and because we have exceptional domain experience and stay
+				current with industry trends, we can build reliable web apps that bring value to our clients' daily operations.
+			</div>
+
+		</div>
+	</div>
 </section>
-<section class="section is-medium">
-	<h1 class="title">Medium section</h1>
-	<h2 class="subtitle">
-		A simple container to divide your page into <strong>sections</strong>, like the one you're
-		currently reading.
-	</h2>
-</section>
+<!--<section class="section is-medium">-->
+<!--	<div id="slider2">-->
+<!--		<div class="card">-->
+<!--			<div class="card-image">-->
+<!--				<figure class="image is-16by9  is-covered">-->
+<!--					<img src="https://images.unsplash.com/photo-1550921082-c282cdc432d6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"-->
+<!--						 alt="">-->
+<!--				</figure>-->
+<!--			</div>-->
+<!--			<div class="card-content">-->
+<!--				<div class="item__title">-->
+<!--					Mon titre 1-->
+<!--				</div>-->
+<!--				<div class="item__description">-->
+<!--					Ici une petite description pour tester le slider-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+
+<!--		<div class="card">-->
+<!--			<div class="card-image">-->
+<!--				<figure class="image is-16by9  is-covered">-->
+<!--					<img src="https://images.unsplash.com/photo-1550945771-515f118cef86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"-->
+<!--						 alt="">-->
+<!--				</figure>-->
+<!--			</div>-->
+<!--			<div class="card-content">-->
+<!--				<div class="item__title">-->
+<!--					Mon titre 2-->
+<!--				</div>-->
+<!--				<div class="item__description">-->
+<!--					Ici une petite description pour tester le slider-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+
+<!--		<div class="card">-->
+<!--			<div class="card-image">-->
+<!--				<figure class="image is-16by9  is-covered">-->
+<!--					<img src="https://images.unsplash.com/photo-1550971264-3f7e4a7bb349?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"-->
+<!--						 alt="">-->
+<!--				</figure>-->
+<!--			</div>-->
+<!--			<div class="card-content">-->
+<!--				<div class="item__title">-->
+<!--					Mon titre 3-->
+<!--				</div>-->
+<!--				<div class="item__description">-->
+<!--					Ici une petite description pour tester le slider-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+
+<!--		<div class="card">-->
+<!--			<div class="card-image">-->
+<!--				<figure class="image is-16by9  is-covered">-->
+<!--					<img src="https://images.unsplash.com/photo-1550931937-2dfd45a40da0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"-->
+<!--						 alt="">-->
+<!--				</figure>-->
+<!--			</div>-->
+<!--			<div class="card-content">-->
+<!--				<div class="item__title">-->
+<!--					Mon titre 4-->
+<!--				</div>-->
+<!--				<div class="item__description">-->
+<!--					Ici une petite description pour tester le slider-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+
+<!--		<div class="card">-->
+<!--			<div class="card-image">-->
+<!--				<figure class="image is-16by9 is-covered">-->
+<!--					<img src="https://images.unsplash.com/photo-1550930516-af8b8cc4f871?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"-->
+<!--						 alt="">-->
+<!--				</figure>-->
+<!--			</div>-->
+<!--			<div class="card-content">-->
+<!--				<div class="item__title">-->
+<!--					Mon titre 5-->
+<!--				</div>-->
+<!--				<div class="item__description">-->
+<!--					Ici une petite description pour tester le slider-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+
+<!--		<div class="card">-->
+<!--			<div class="card-image">-->
+<!--				<figure class="image video-container is-16by9">-->
+<!--					<iframe type="text/html" src="https://www.youtube.com/embed/H0v773vKS_U" frameborder="0"></iframe>-->
+<!--				</figure>-->
+<!--			</div>-->
+<!--			<div class="card-content">-->
+<!--				<div class="item__title">-->
+<!--					Mon titre 6-->
+<!--				</div>-->
+<!--				<div class="item__description">-->
+<!--					Ici une petite description pour tester le slider-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+
+<!--	</div>-->
+<!--</section>-->
