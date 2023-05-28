da95fb7bfad21cddf0b2d557bf361139c83fa017
Thuhaa <josephthuha@gmail.com>
2023-05-28 13:00:33 +0300

    fix: minor fixes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
+	const date = new Date();
+	const year = date.getFullYear();
 </script>
 
 <footer class="footer has-text-white">
 	<div class="content has-text-centered">
 		<p class="text-base hover:text-blue-600 xl:text-center">
 			©
-			<!-- -->2023
-			<!-- -->
-			<!-- -->BlinkCode Solutions
-			<!-- -->. All rights reserved.
+			{year}
+			BlinkCode Solutions All rights reserved.
 		</p>
 	</div>
 </footer>
