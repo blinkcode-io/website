c2b893675817e0937e43d5a8017a1c503f4295d7
Joseph Kariuki <joehene@gmail.com>
2023-06-28 20:48:01 +0300

    Created blog page.

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+</script>
+
+<section class="hero is-primary is-halfheight mt-0">
+	<div class="hero-body">
+		<div>
+			<p class="title">BLOG</p>
+			<p class="subtitle">
+				Tutorials and tips designed to teach critical skills needed to test, launch, scale, and
+				optimize applications.
+			</p>
+		</div>
+	</div>
+</section>
+<section class="section is-medium">
+	<p class="subtitle has-text-centered m-3">LATEST POSTS</p>
+	<nav class="pagination" role="navigation" aria-label="pagination">
+		<a class="pagination-previous is-disabled" title="This is the first page">Previous</a>
+		<a class="pagination-next">Next page</a>
+		<ul class="pagination-list">
+			<li>
+				<a class="pagination-link is-current" aria-label="Page 1" aria-current="page">1</a>
+			</li>
+			<li>
+				<a class="pagination-link" aria-label="Goto page 2">2</a>
+			</li>
+			<li>
+				<a class="pagination-link" aria-label="Goto page 3">3</a>
+			</li>
+		</ul>
+	</nav>
+</section>
